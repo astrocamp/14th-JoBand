2f10b909cb96f1909ebee3edf4ea658bf978d8bc
Weber <vito588p@gmail.com>
2023-08-05 14:13:55 +0800

    feat: add User Model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable
+end
