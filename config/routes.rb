b23b0324219b22f58cbf7c91c3ba8da1b6d7c6c4
Weber <vito588p@gmail.com>
2023-08-07 18:10:06 +0800

    feat: add users registrations and login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations",
+    passwords: "users/passwords"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
+
+  resources :pages, only: [:index]
+
 end
