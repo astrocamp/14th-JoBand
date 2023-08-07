41ff9ac2e8a063116882346528a1f33c77c7c44d
Hao <liangyuhao818@gmail.com>
2023-08-08 01:14:16 +0800

    feat: add Band's CRU

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,7 @@
 class Band < ApplicationRecord
   validates :name ,presence: true
 
+<<<<<<< HEAD
   has_many :band_styles
   has_many :styles, through: :band_styles
 
@@ -9,14 +10,61 @@ class Band < ApplicationRecord
     recruiting: 1,  # 招募中
     on_break: 2,    # 暫時休團
     disbanded: 3    # 解散
+=======
+  enum state: {
+    active: 0,          # 活躍中
+    recruiting: 1,      # 招募中
+    on_break: 2,        # 暫時休團
+    disbanded: 3        # 解散
+>>>>>>> 54d4f6e (feat: add Band's CRU)
   }, _prefix: true
 
   enum area: {
     North: 0,
     Central: 1,
+<<<<<<< HEAD
     South: 2,
     East: 3,
   },_prefix: true
+=======
+    south: 2,
+    north: 3,
+  },_prefix: true
+  
+    #表單序列化
+    serialize :styles, Array  
+    def self.styles
+        [
+        "Pop",
+        "Folk",
+        "Rock",
+        "Hip-hop",
+        "Metal",
+        "Jazz",
+        "Classical",
+        "Electronic"
+      ]
+    end
+
+    before_save :set_styles
+
+    def set_styles
+      self.styles = self.styles.reject{|e| e.empty? }
+    end
+  # enum styles: {
+  #   pop: 0,       #流行
+  #   folk: 1,      #民謠
+  #   rock: 2,      #搖滾
+  #   hip_hop: 3,   #嘻哈
+  #   metal: 4,     #金屬
+  #   jazz: 5,      #爵士
+  #   classical: 6, #古典
+  #   elecronic: 7  #電子
+  # }, _prefix: true
+
+  
+
+>>>>>>> 54d4f6e (feat: add Band's CRU)
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,31 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_025201) do
     t.string "google_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_08_05_101630) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bands", force: :cascade do |t|
+    t.string "name"
+    t.string "styles"
+    t.text "content"
+    t.integer "area"
+    t.integer "state"
+    t.date "founded_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "band_styles", "bands"
