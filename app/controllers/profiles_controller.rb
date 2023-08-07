0e2319f8da7f580f7ea50b28ab66c9203ef5c19c
Leo Chang <playingwood@gmail.com>
2023-08-07 17:43:20 +0800

    feat: create profile with routes, controller, model and view

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,27 @@
+class ProfilesController < ApplicationController
+    
+    def index
+        
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        
+    end
+    
+    def show
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+end
---
+++ db/migrate/20230807092912_create_profiles.rb
@@ -0,0 +1,13 @@
+class CreateProfiles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :profiles do |t|
+      t.string :name
+      t.integer :phone
+      t.string :location
+      t.integer :seniority
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
