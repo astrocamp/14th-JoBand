3cf741416fe2c39ca14359e5062550517c08aeaf
Hao <liangyuhao818@gmail.com>
2023-08-08 18:45:12 +0800

    style: Code Tidying

--- app/models/band.rb
+++ app/models/band.rb
@@ -12,16 +12,24 @@ class Band < ApplicationRecord
     disbanded: 3    # 解散
 =======
   enum state: {
+<<<<<<< HEAD
     active: 0,          # 活躍中
     recruiting: 1,      # 招募中
     on_break: 2,        # 暫時休團
     disbanded: 3        # 解散
 >>>>>>> 54d4f6e (feat: add Band's CRU)
+=======
+    active: 0,      # 活躍中
+    recruiting: 1,  # 招募中
+    on_break: 2,    # 暫時休團
+    disbanded: 3    # 解散
+>>>>>>> be8e526 (style: Code Tidying)
   }, _prefix: true
 
   enum area: {
     North: 0,
     Central: 1,
+<<<<<<< HEAD
 <<<<<<< HEAD
     South: 2,
     East: 3,
@@ -29,6 +37,10 @@ class Band < ApplicationRecord
 =======
     south: 2,
     north: 3,
+=======
+    South: 2,
+    East: 3,
+>>>>>>> be8e526 (style: Code Tidying)
   },_prefix: true
   
     #表單序列化
@@ -51,16 +63,7 @@ class Band < ApplicationRecord
     def set_styles
       self.styles = self.styles.reject{|e| e.empty? }
     end
-  # enum styles: {
-  #   pop: 0,       #流行
-  #   folk: 1,      #民謠
-  #   rock: 2,      #搖滾
-  #   hip_hop: 3,   #嘻哈
-  #   metal: 4,     #金屬
-  #   jazz: 5,      #爵士
-  #   classical: 6, #古典
-  #   elecronic: 7  #電子
-  # }, _prefix: true
+  
 
   
 
