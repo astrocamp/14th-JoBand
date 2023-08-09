753f343f168e0e7d8dec5f20517944584dc59eca
Leo Chang <playingwood@gmail.com>
2023-08-09 11:54:02 +0800

    feat: add nested forms for profile & instrument

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,10 +5,17 @@ class ProfilesController < ApplicationController
     end
     
     def new
-        
+        @profile = Profile.new
+        @profile.instruments.new
     end
     
     def create
+        @profile = Profile.new(params_profile)
+        if @profile.save
+            redirect_to profiles_path, notice: "新增 profile 成功"
+        else
+            render :new
+        end
         
     end
     
@@ -22,6 +29,10 @@ class ProfilesController < ApplicationController
     
     def update
         
+    end
+
+    def params_profile
+        params.require(:profile).permit(:name, :phone, :location, :seniority, :content, instruments_attributes: [:vocal, :guitar, :bass, :keyboard, :drum, :others])
     end
     
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,4 @@
 class Profile < ApplicationRecord
   has_many :instruments
+  accepts_nested_attributes_for :instruments, allow_destroy: true
 end
