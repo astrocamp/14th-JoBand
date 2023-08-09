7c32021d974ea5b38f872680f628f9268dc7aade
Leo Chang <playingwood@gmail.com>
2023-08-09 11:25:28 +0800

    feat: add instrument model and association with profile

---
+++ app/models/instrument.rb
@@ -0,0 +1,3 @@
+class Instrument < ApplicationRecord
+  belongs_to :profile
+end
---
+++ db/migrate/20230809030758_create_instruments.rb
@@ -0,0 +1,15 @@
+class CreateInstruments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :instruments do |t|
+      t.boolean :vocal
+      t.boolean :guitar
+      t.boolean :bass
+      t.boolean :keyboard
+      t.boolean :drum
+      t.string :others
+      t.belongs_to :profile, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
