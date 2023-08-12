0e546809b46241c5a67f7b0db7f80f6f65d53cb5
Hao <liangyuhao818@gmail.com>
2023-08-13 01:25:17 +0800

    refactor: Update Band-Style relationship

--- app/models/band.rb
+++ app/models/band.rb
@@ -5,15 +5,37 @@ class Band < ApplicationRecord
   has_many :styles, through: :band_styles
 
   enum state: {
-    active: 0,      # 活躍中
-    recruiting: 1,  # 招募中
-    on_break: 2,    # 暫時休團
-    disbanded: 3    # 解散
-
-  enum state: {
     active: 0,          # 活躍中
     recruiting: 1,      # 招募中
     on_break: 2,        # 暫時休團
     disbanded: 3        # 解散
+  }, _prefix: true
 
+  enum area: {
+    North: 0,
+    Central: 1,
+    South: 2,
+    East: 3,
+  },_prefix: true
+  
+    #表單序列化
+    serialize :styles, Array  
+    def self.styles
+        [
+        "Pop",
+        "Folk",
+        "Rock",
+        "Hip-hop",
+        "Metal",
+        "Jazz",
+        "Classical",
+        "Electronic"
+      ]
+    end
+
+    before_save :set_styles
+
+    def set_styles
+      self.styles = self.styles.reject{|e| e.empty? }
+    end
 end
