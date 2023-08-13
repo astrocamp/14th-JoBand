3257cafa2f6f6590bf1643eb7552f6baeed537c2
James Tung <140575755+codzilla91@users.noreply.github.com>
2023-08-13 10:24:20 +0800

    新增Google & Facebook 第三方登入 feat: add OAuth -login (#9)
    * feat: add OAuth -login
    
    * Delete settings.json
    
    刪除本機 vscode 設定檔
    
    * refactor: Update Gemfile, move dotenv-rails to dev and test envirment
    
    Update Gemfile, move dotenv-rails to dev and test environment.
    
    from #6 -> #40
    
    ---------
    
    Co-authored-by: Weber <128116961+vito588p@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema[7.0].define(version: 2023_08_05_055741) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_025201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -11,6 +23,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_05_055741) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fb_uid"
+    t.string "fb_token"
+    t.string "google_uid"
+    t.string "google_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
