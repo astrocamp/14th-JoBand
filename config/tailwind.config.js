df826dc058caa533989655ac18ed4314f6ced453
Weber <128116961+vito588p@users.noreply.github.com>
2023-08-13 08:31:21 +0800

    style: 登入/註冊＆navbar 畫面切版 （附加：中英語內容yml) (#8)
    * style: styling login & sign_up pages
    
    * style: add dropdown to navbar and restyled it
    
    * docs: add more content i18n in en/tw login signup

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,11 +7,24 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
+  
   theme: {
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+      '2xl': ['30px', '36px'],
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'dark': '#444',
+        'nav-hov': '#2f3136',
+      },
+      
     },
   },
   plugins: [
