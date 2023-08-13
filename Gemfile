fa73021dea1a705287d0a51bd981fe2dc91b0dc6
James Tung <140575755+codzilla91@users.noreply.github.com>
2023-08-13 10:24:20 +0800

    新增Google & Facebook 第三方登入 feat: add OAuth -login (#9)
    * feat: add OAuth -login
    
    * Delete settings.json
    
    刪除本機 vscode 設定檔
    
    * refactor: Update Gemfile, move dotenv-rails to dev and test envirment
    
    Update Gemfile, move dotenv-rails to dev and test environment.
    
    from #6 -> #40
    
    ---------
    
    Co-authored-by: Weber <128116961+vito588p@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 gem "devise", "~> 4.9"
 gem "omniauth", "~> 2.1"
+gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
+gem 'omniauth-rails_csrf_protection'
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
@@ -38,6 +41,7 @@ gem "omniauth", "~> 2.1"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+  gem 'dotenv-rails'
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_12_063037) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_025201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_12_063037) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fb_uid"
+    t.string "fb_token"
+    t.string "google_uid"
+    t.string "google_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
