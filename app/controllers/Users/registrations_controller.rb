3d7dc89945037dbb9004561515781ed4e0db64e8
Weber <vito588p@gmail.com>
2023-08-17 16:53:21 +0800

    feat:adding user name in the columns&views, mails

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -2,8 +2,8 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
-    # before_action :configure_sign_up_params, only: [:create]
-    # before_action :configure_account_update_params, only: [:update]
+    before_action :configure_sign_up_params, only: [:create]
+    before_action :configure_account_update_params, only: [:update]
 
     # GET /resource/sign_up
 
@@ -30,14 +30,14 @@ module Users
     # protected
 
     # If you have extra params to permit, append them to the sanitizer.
-    # def configure_sign_up_params
-    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-    # end
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 
     # If you have extra params to permit, append them to the sanitizer.
-    # def configure_account_update_params
-    #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-    # end
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 
     # The path used after sign up.
     # def after_sign_up_path_for(resource)
