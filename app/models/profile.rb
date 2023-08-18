9aa195c9982869a11e35e3e1bfbd9430d559ea32
heng <liokyysz@gmail.com>
2023-08-18 12:55:17 +0800

    feat: profile upload music

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,6 +7,7 @@ class Profile < ApplicationRecord
 
   # associations
   has_one_attached :avatar
+  has_one_attached :music
   has_many :profile_and_instrument
   has_many :instruments, through: :profile_and_instrument
 end
