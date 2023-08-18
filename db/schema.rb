842f724da497a7d08747429efd27c4b317fe5934
leo chang <leochang0808@gmail.com>
2023-08-18 19:53:15 +0800

    style: add Joband index video and rubocop files

--- db/schema.rb
+++ db/schema.rb
@@ -97,8 +97,6 @@ ActiveRecord::Schema[7.0].define(version: 20_230_819_161_600) do
     t.text 'content'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.index ['user_id'], name: 'index_profiles_on_user_id'
   end
 
   create_table 'recruit_and_instruments', force: :cascade do |t|
@@ -128,7 +126,7 @@ ActiveRecord::Schema[7.0].define(version: 20_230_819_161_600) do
   end
 
   create_table 'users', force: :cascade do |t|
-    t.string 'name', null: false
+    t.string 'name'
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
     t.string 'reset_password_token'
