681314ced6ee38c6bbe628618838cec2da4e997f
fr715464 <fr715464@gmail.com>
2023-08-19 23:48:18 +0800

    fix: remove band id for recruit schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -26,56 +24,56 @@ ActiveRecord::Schema[7.0].define(version: 20_230_817_120_153) do
                                                     unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum'
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'band_styles', force: :cascade do |t|
-    t.bigint 'band_id', null: false
-    t.bigint 'style_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['band_id'], name: 'index_band_styles_on_band_id'
-    t.index ['style_id'], name: 'index_band_styles_on_style_id'
+  create_table "band_styles", force: :cascade do |t|
+    t.bigint "band_id", null: false
+    t.bigint "style_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_band_styles_on_band_id"
+    t.index ["style_id"], name: "index_band_styles_on_style_id"
   end
 
-  create_table 'bands', force: :cascade do |t|
-    t.string 'name'
-    t.text 'content'
-    t.integer 'area'
-    t.integer 'state'
-    t.date 'founded_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "bands", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "area"
+    t.integer "state"
+    t.date "founded_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'instruments', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "instruments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'profile_and_instruments', force: :cascade do |t|
-    t.bigint 'profile_id', null: false
-    t.bigint 'instrument_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['instrument_id'], name: 'index_profile_and_instruments_on_instrument_id'
-    t.index ['profile_id'], name: 'index_profile_and_instruments_on_profile_id'
+  create_table "profile_and_instruments", force: :cascade do |t|
+    t.bigint "profile_id", null: false
+    t.bigint "instrument_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["instrument_id"], name: "index_profile_and_instruments_on_instrument_id"
+    t.index ["profile_id"], name: "index_profile_and_instruments_on_profile_id"
   end
 
   create_table 'profiles', force: :cascade do |t|
@@ -90,29 +88,28 @@ ActiveRecord::Schema[7.0].define(version: 20_230_817_120_153) do
     t.index ['user_id'], name: 'index_profiles_on_user_id'
   end
 
-  create_table 'recruit_and_instruments', force: :cascade do |t|
-    t.bigint 'recruit_id', null: false
-    t.bigint 'instrument_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['instrument_id'], name: 'index_recruit_and_instruments_on_instrument_id'
-    t.index ['recruit_id'], name: 'index_recruit_and_instruments_on_recruit_id'
+  create_table "recruit_and_instruments", force: :cascade do |t|
+    t.bigint "recruit_id", null: false
+    t.bigint "instrument_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["instrument_id"], name: "index_recruit_and_instruments_on_instrument_id"
+    t.index ["recruit_id"], name: "index_recruit_and_instruments_on_recruit_id"
   end
 
-  create_table 'recruits', force: :cascade do |t|
-    t.string 'recruit_title'
-    t.text 'condition'
-    t.string 'practice_time'
-    t.integer 'area'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'band_id'
+  create_table "recruits", force: :cascade do |t|
+    t.string "recruit_title"
+    t.text "condition"
+    t.string "practice_time"
+    t.integer "area"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'styles', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table 'users', force: :cascade do |t|
@@ -136,12 +133,12 @@ ActiveRecord::Schema[7.0].define(version: 20_230_817_120_153) do
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'band_styles', 'bands'
-  add_foreign_key 'band_styles', 'styles'
-  add_foreign_key 'profile_and_instruments', 'instruments'
-  add_foreign_key 'profile_and_instruments', 'profiles'
-  add_foreign_key 'recruit_and_instruments', 'instruments'
-  add_foreign_key 'recruit_and_instruments', 'recruits'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "band_styles", "bands"
+  add_foreign_key "band_styles", "styles"
+  add_foreign_key "profile_and_instruments", "instruments"
+  add_foreign_key "profile_and_instruments", "profiles"
+  add_foreign_key "recruit_and_instruments", "instruments"
+  add_foreign_key "recruit_and_instruments", "recruits"
 end
