8b8edfae62b1b8840a57ba604b194b8a75f83d24
fr715464 <fr715464@gmail.com>
2023-08-20 00:24:22 +0800

    docs: add reference band id for recruit

---
+++ db/migrate/20230819161600_add_band_id_to_recruits.rb
@@ -0,0 +1,5 @@
+class AddBandIdToRecruits < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :recruits, :band, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -104,6 +104,8 @@ ActiveRecord::Schema[7.0].define(version: 20_230_817_120_153) do
     t.integer "area"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "band_id"
+    t.index ["band_id"], name: "index_recruits_on_band_id"
   end
 
   create_table "styles", force: :cascade do |t|
@@ -141,4 +143,5 @@ ActiveRecord::Schema[7.0].define(version: 20_230_817_120_153) do
   add_foreign_key "profile_and_instruments", "profiles"
   add_foreign_key "recruit_and_instruments", "instruments"
   add_foreign_key "recruit_and_instruments", "recruits"
+  add_foreign_key "recruits", "bands"
 end
