b41f45854dd61ad4f56f81af05397534a852ad10
Hao <liangyuhao818@gmail.com>
2023-08-23 00:35:04 +0800

    feat: add Band's link to Profile show

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 
   # associations
   has_one :profile
+  has_many :band_members
+  has_many :bands, through: :band_members
 
   def self.find_for_google_oauth2(access_token, _signed_in_resource = nil)
     data = access_token.info
