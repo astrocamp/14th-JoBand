59d46e7aca3c0fef77bcfe9544beba274fc8c251
heng <liokyysz@gmail.com>
2023-08-24 15:39:26 +0800

    fix : recruits update instruments_all

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+    def instruments_all
+        @instruments = Instrument.all
+    end
 end
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 module ProfilesHelper
-  def instruments_all
-    @instruments = Instrument.all
-  end
 end
