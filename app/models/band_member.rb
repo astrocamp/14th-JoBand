72b109dcdff71f7fbf64b13e7067ad0d41d6f3e7
Hao <liangyuhao818@gmail.com>
2023-08-24 16:00:53 +0800

    feat: chang column in band_member

--- app/models/band_member.rb
+++ app/models/band_member.rb
@@ -4,5 +4,15 @@ class BandMember < ApplicationRecord
   belongs_to :user
   belongs_to :band
 
-  enum band_role: %i[leader manager member]
+  enum identity: %i[leader manager member]
+  
+  def self.default_roles
+    [
+     'Guitar',
+     'Bass',
+     'Vocal',
+     'Drum',
+     'Keyboard'
+    ]
+  end
 end
---
+++ db/migrate/20230824070202_change_band_members_columns.rb
@@ -0,0 +1,6 @@
+class ChangeBandMembersColumns < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :band_members, :band_role, :identity
+    add_column :band_members, :role, :string
+  end
+end
