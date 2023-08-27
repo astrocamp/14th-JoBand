459ee41babc3a6bf64214edcd84cd5ea1141082e
Weber <vito588p@gmail.com>
2023-08-27 22:55:52 +0800

    build: build in ESbuild, remove importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
 import "trix"
 import "@rails/actiontext"
 import 'sweetalert2'
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,6 +10,11 @@ module.exports = {
   
   theme: {
     extend: {
+      fontFamily: {
+        sans: [ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+        serif: [ui-serif, Georgia, Cambria, "Times New Roman", Times, serif],
+        mono: [ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace]
+      },
       colors: {
         'dark': '#444',
         'nav-hov': '#2f3136',
@@ -18,11 +23,11 @@ module.exports = {
       },
     },
   },
-  // plugins: [
-  //   require('@tailwindcss/forms'),
-  //   require('@tailwindcss/aspect-ratio'),
-  //   require('@tailwindcss/typography'),
-  //   require('@tailwindcss/container-queries'),
-  // ],
-  // plugins: [require("daisyui")],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ],
+  plugins: [require("daisyui")],
 }
