9a0b83862a5288caf1bca4aef9178ecbdff87490
Weber <vito588p@gmail.com>
2023-08-27 23:20:57 +0800

    fix: fix font family style syntax

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,9 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: [ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-        serif: [ui-serif, Georgia, Cambria, "Times New Roman", Times, serif],
-        mono: [ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace]
+        sans: ['Noto Sans TC'],
       },
       colors: {
         'dark': '#444',
