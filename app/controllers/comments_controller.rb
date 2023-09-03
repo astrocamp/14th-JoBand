04dabd8a55e93a13df4529b2e58ad94ca6fbab15
fr715464 <fr715464@gmail.com>
2023-09-03 15:12:19 +0800

    fix:comment controller revise

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,13 +9,13 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to @commentable, notice: '留言成功'
     else
-      redirect_to @commentable, alert: '留言不得為空白'
+      render :show
     end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-    @comment.update(deleted_at: Time.now)
+    @comment.destroy
     redirect_to @commentable, notice: '留言已刪除'
   end
 
