7ef81b235f4a4d196a90bd5d3d4ce4db25564404
James <jtung501@gmail.com>
2023-09-04 15:55:10 +0800

    style: add friendly id to replace band id

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Band < ApplicationRecord
+  #friendly_id
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+  
   validates :name, presence: true
 
   # associations
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :activities
 
-  resources :bands, except: [:destroy] do
+  resources :bands, param: :slug, except: [:destroy] do
     resources :band_members, except: %i[new show index]
     resources :recruits, shallow: true do
       resources :resume_lists, except: %i[destroy index] do
