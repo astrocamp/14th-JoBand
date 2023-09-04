9fa4508c08951987497d45f4997e70052f403f91
Weber <vito588p@gmail.com>
2023-09-04 21:16:03 +0800

    fix: add email host

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ Bundler.require(*Rails.groups)
 module Joband
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.action_mailer.default_url_options = { host: ENV["DOMAIN"] }
     config.load_defaults 7.0
     config.i18n.default_locale = :tw
     I18n.available_locales = [:tw, :en]
