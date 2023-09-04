2b2bd3c87f583c9994a4626a0e43ad68b91b3014
Weber <vito588p@gmail.com>
2023-09-04 11:30:13 +0800

    feat: setup database and config, ready for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << ENV["DOMAIN"]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
