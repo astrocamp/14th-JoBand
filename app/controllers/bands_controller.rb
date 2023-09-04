6f71c01f13c0efc09d420ace3096ed8e9ad237c8
James <jtung501@gmail.com>
2023-09-04 17:24:30 +0800

    fix: add band name uniqueness validates

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -24,7 +24,8 @@ class BandsController < ApplicationController
       @band.band_members.create(user: current_user, identity: :leader, role: @role)
       redirect_to band_path(@band), notice: '成功創立樂團'
     else
-      render :new, notice: '失敗'
+      flash.now[:alert] = '創建樂團失敗，樂團名稱重複。'
+      render :new
     end
   end
 
