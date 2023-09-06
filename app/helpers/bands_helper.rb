357ab8f1eb32ee66cadb2a7a11fd676a751332a0
Hao <liangyuhao818@gmail.com>
2023-09-06 21:14:43 +0800

    feat: add 403 page

--- app/helpers/bands_helper.rb
+++ app/helpers/bands_helper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module BandsHelper
-  def bnad_leader
+  def band_leader
     @band.band_members.find_by(identity: :leader).user
   end
 
-  def bnad_manager
+  def band_manager
     @band.band_members.find_by(identity: :manager)&.user
   end
 end
