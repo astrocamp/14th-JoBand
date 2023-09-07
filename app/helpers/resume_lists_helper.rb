be19ed042a0fb7a0deabf7f34d2a6eb55a927137
leo chang <leochang0808@gmail.com>
2023-09-07 17:20:29 +0800

    docs: remove Instruments_all, change to Instrument.all

--- app/helpers/resume_lists_helper.rb
+++ app/helpers/resume_lists_helper.rb
@@ -2,6 +2,6 @@
 
 module ResumeListsHelper
   def recruits_roles
-    instruments_all.where(id: @recruit.instrument_ids).pluck(:name)
+    Instrument.all.where(id: @recruit.instrument_ids).pluck(:name)
   end
 end
