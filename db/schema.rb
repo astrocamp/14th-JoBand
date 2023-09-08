a52eb7337a41182d3ff88d819b74cd96105ab77f
fr715464 <fr715464@gmail.com>
2023-09-08 11:53:18 +0800

    fix:activity index revise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_072633) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_214424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -128,6 +128,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_072633) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "like_logs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_like_logs_on_post_id"
+    t.index ["user_id"], name: "index_like_logs_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "deleted_at"
@@ -224,6 +233,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_072633) do
   add_foreign_key "band_styles", "bands"
   add_foreign_key "band_styles", "styles"
   add_foreign_key "comments", "users"
+  add_foreign_key "like_logs", "posts"
+  add_foreign_key "like_logs", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "profile_and_instruments", "instruments"
   add_foreign_key "profile_and_instruments", "profiles"
