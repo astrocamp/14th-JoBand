8bab41a8a806e314d399dd08ec3f30cd68b0a089
Weber <vito588p@gmail.com>
2023-09-10 21:11:15 +0800

    feat: add random 30 order_number

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,11 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :band
 
+
+  def generate_tracking_number
+    timestamp = Time.now.strftime("%Y%m%d%H%M%S")
+    random_part = SecureRandom.alphanumeric(30 - timestamp.length)
+    self.tracking_number = "#{timestamp}#{random_part}"
+  end
+
 end
