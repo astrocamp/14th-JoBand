fa14b6d0fac136810aa5256deb1af46e60b6519d
Weber <vito588p@gmail.com>
2023-09-11 21:25:11 +0800

    feat: make sure i18n stick to tw & en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,9 @@ class ApplicationController < ActionController::Base
   end
 
   def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
+    locale = session[:locale] || params[:locale] || I18n.default_locale
+    locale = 'tw' unless I18n.available_locales.include?(locale.to_sym)
+    I18n.with_locale(locale, &action) 
   end
 
   def default_url_options
