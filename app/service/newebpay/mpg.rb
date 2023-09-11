7db56b6444b3808368cb6f2b0ee52e8cb390ed8f
Weber <vito588p@gmail.com>
2023-09-11 22:26:56 +0800

    docs: replace domain name

--- app/service/newebpay/mpg.rb
+++ app/service/newebpay/mpg.rb
@@ -31,7 +31,7 @@ module Newebpay
       info[:Amt] = order[:Amt]
       info[:ItemDesc] = order[:ItemDesc]
       info[:Email] = order[:Email]
-      info[:ReturnURL] = "https://268d-61-220-182-115.ngrok-free.app/bands/#{order[:Band].slug}/orders/paid"
+      info[:ReturnURL] = "ENV["DOMAIN"]/#{order[:Band].slug}/orders/paid"
       info[:LoginType] = 0 
       info[:CREDIT] =  1
       info[:VACC] = 1
