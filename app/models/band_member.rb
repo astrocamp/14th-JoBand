f1461cdc13c0f14a2ad4040f2c8db27e0adb9151
James <jtung501@gmail.com>
2023-09-14 11:59:47 +0800

    feat: add apply notice

--- app/models/band_member.rb
+++ app/models/band_member.rb
@@ -4,6 +4,23 @@ class BandMember < ApplicationRecord
   belongs_to :user
   belongs_to :band
 
+  after_create_commit :notify_recipient
+  before_destroy :cleanup_notifications
+  has_noticed_notifications model_name: 'Notification'
+
+  def notify_recipient
+    
+    applyer = resume_list.user
+
+    return if applyer == user
+
+    BandMemberNotification.with(BandMember: self, resume_list: resume_list).deliver_later(applyer)
+  end
+
+  def cleanup_notifications
+    notifications_as_resume_list.destroy_all
+  end
+
   enum identity: %i[leader manager member]
 
   def self.default_roles
