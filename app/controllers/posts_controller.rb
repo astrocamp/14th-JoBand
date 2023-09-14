3c56d1c147ad1316bcefdba7672eb7fe73ade726
Weber <vito588p@gmail.com>
2023-09-14 22:02:02 +0800

    feat: add differ channel

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:body)
+    params.require(:post).permit(:body, :channel_id)
   end
 
   def set_post
