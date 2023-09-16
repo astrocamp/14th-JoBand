d446b958d16e296f99c7165f49ccbfce4eff555a
Weber <vito588p@gmail.com>
2023-09-16 08:19:55 +0800

    docs: add meta-title, description

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,9 @@ class PagesController < ApplicationController
   def index; end
 
   def about; end
+
+  def robots
+    robots = File.read(Rails.root + "config/robots.txt")
+    render :text => robots, :layout => false, :content_type => "text/plain"
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title: "JoBand 揪團",
+      description: "專屬音樂人的社群平台。尋找合適的樂團成員，開始您的音樂之旅。",
+      keywords: "樂團, 樂手, 組團, band, 音樂, 鼓手, 吉他, 貝斯, 主唱, 音樂祭",
+      separator: "&mdash;".html_safe
+      set_meta_tags icon: [
+        {href: "/images/favicon.ico" },
+      set_meta_tags canonical: "http://joband.co/canonical"
+    }
+  end
 end
