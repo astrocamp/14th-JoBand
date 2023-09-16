f0914fc5ad398b7c47692bd55e1b878d3c69b071
Weber <vito588p@gmail.com>
2023-09-16 09:04:36 +0800

    fix: docs logic fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ module ApplicationHelper
   def default_meta_tags
     {
       title: "JoBand 揪團",
-      description: "專屬音樂人的社群平台。尋找合適的樂團成員，開始您的音樂之旅。",
+      description: "Joband是一個專為音樂人打造的社群平台，旨在協助您輕鬆尋找合適的樂團成員，開始您的音樂之旅。我們歡迎各種音樂才華，無論是鼓手、吉他手、主唱、貝斯手，還是其他音樂領域的人才。加入Joband，與全球的音樂人一起分享音樂夢想，開啟您的音樂旅程！",
       keywords: "樂團, 樂手, 組團, band, 音樂, 鼓手, 吉他, 貝斯, 主唱, 音樂祭",
       icon: [{ href: "/images/favicon.ico" }],
-      canonical: "http://joband.co/canonical"
+      canonical: "https://joband.co/"
     }
   end
 end
