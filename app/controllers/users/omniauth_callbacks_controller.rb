914b28d10ef38cc10dbd6257e4d2b4b757fd62d7
Weber <vito588p@gmail.com>
2023-09-16 11:24:12 +0800

    hotfix: omniauth login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,7 @@ module Users
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: provider
-        redirect_to welcome_profiles_path
+        sign_in_and_redirect @user, event: :authentication
       else
         session['devise.auth_data'] = request.env['omniauth.auth'].except('extra')
         redirect_to welcome_profiles_path, alert: @user.errors.full_messages.join("\n")
